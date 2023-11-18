b1893e110c8f516f33bc6b228a582790849bc2c1
Abdullmajeed Al-Sabqi <134193752+A-gitc@users.noreply.github.com>
2023-11-18 17:13:50 +0400

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,15 @@
+﻿namespace Day_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that takes a number from the user then print yes
+            //if that number can be divided by 3 and 4 otherwise print no.
+
+            Console.Write("Enter a number to check if the number divisible by 3 and 4: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine((num % 3 == 0 && num % 4 == 0) ? "Yes" : "No");
+        }
+    }
+}
