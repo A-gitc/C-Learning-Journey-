8ba5559655f45ec1e06fb13099f0d0af296d67cc
Abdullmajeed Al-Sabqi <134193752+A-gitc@users.noreply.github.com>
2023-11-18 16:24:24 +0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,97 +1,99 @@
-﻿namespace Day_4
+﻿namespace Day_2_example_code
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Array
-            //example
+           // Write a program for creating a Callculator
 
-            int[] numbers = new int[5] { 1, 2, 3, 4, 5  };
-
-            //Array Declaration
-
-            int[] arr = new int[3];
-
-            //Innitalization
-
-            arr[0] = 1;
-            arr[1] = 2;
-            arr[2] = 3;
-
-            //Initialization and Declaration in One Line
-
-            int[] arr1 = new int[3] { 3, 4, 5, };
-
-            //Syntax sugar for array 
-
-            string[] strings = { "Ahmed", "Sara", "Hamad" };
-
-
-            //Multi=diemensonal array
-
-            int[,] matrix = new int[3, 3];
-
-            //declare and initialize a two-diemensional integer array with diemensions 3*3
-
-            matrix[0, 0] = 1;
-            matrix[0, 1] = 2;
-            matrix[1, 0] = 3;
-            matrix[1, 1] = 4;
-            matrix[2, 0] = 5;
-            matrix[2, 1] = 6;
-
-            //Jagged Array
-
-            int[][] jaggedArray = new int[3][];
-            jaggedArray[0] = new int[] { 1, 2, 3 };
-            jaggedArray[1] = new int[] { 4,5 };
-            jaggedArray[2] = new int[] { 6, 7, 8, 9};
-
-
-            //Jagged Array using initializer list
-
-            int[][] jaggedArray1 = new int[][]
+            ﻿namespace HomeWork1
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
             {
-                new int[] {1, 2, 3, },
-                new int[] {4,5},
-                new int[] {6,7,8,9}
+                List<string> list_drinks = new List<string> { "Americano", "Latte", "Cappuccino" };
+                List<string> list_custimaization = new List<string> { "Small", "Medium", "Large" };
+                List<string> list_price = new List<string> { "2.50", "3.00", "3.50" };
 
-            };
+                string menu = " Welcome to the Coffee Shop!\n " +
+                    "Menu:\n" +
+                    "   1.Americano - $2.50\n" +
+                    "   2.Latte - $3.00\n" +
+                    "   3.Cappuccino - $3.50\n" +
+                    "Select a coffee(1-3): ";
+                Console.WriteLine(menu);
+                String type = Console.ReadLine();
+                String custimaizaion = "Custimaizaion:\n" +
+                    "   1.Small\n" +
+                    "   2.Medium\n" +
+                    "   3.Large\n" +
+                    "Select a Size (1-3): ";
+                Console.Write(custimaizaion);
+                String size = Console.ReadLine();
+                Console.Write("Do you want Suger? (yes/no): ");
+                string suger = Console.ReadLine();
+                Console.Write("Do you want milk? (yes/no): ");
+                string milk = Console.ReadLine();
+                if (milk == "yes" && suger == "no")
+                {
+                    Console.WriteLine("Your Order Summary: " + list_drinks[int.Parse(type) - 1] +
+                        "(" + list_custimaization[int.Parse(size) - 1] + ")" + " with Milk");
 
 
-            //Array Slicing
+                }
+                else if (milk == "no" && suger == "yes")
+                {
+                    Console.WriteLine("Your Order Summary: " + list_drinks[int.Parse(type) - 1] +
+                        "(" + list_custimaization[int.Parse(size)] + ")" + " with Suger");
 
-            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
-            int[] slice = numbers[1..4];
-            Console.WriteLine(slice[0]);
+                }
+                else
+                {
+                    Console.WriteLine("Your Order Summary: " + list_drinks[int.Parse(type) - 1] +
+                       "(" + list_custimaization[int.Parse(size)] + ")" + " with Milk and Suger");
+
+                }
+                Console.WriteLine("Total Cost: $" + list_price[int.Parse(type) - 1]);
+                Console.WriteLine("Thanks you for ordering!");
 
 
-            //Reverse Slicing
-
-            var slice3 = numbers[2, ..2 ^];
-            Console.WriteLine(slice3[0]);
 
 
-            //Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
 
-            int[] ls1 = { 1, 2, 3, 4, 5, 6 };
-            int[] ls2 = { 7, 8, 9, 10, 11, 12 };
-            ls1 = ls1.Concat(ls2).ToArray();
-            Array.Sort(ls1);
-            foreach (int j in ls1)
-            {
-                Console.Write(j + " ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                Console.WriteLine("Hello, World!");
             }
-
-
-
-
-
-
-
-
-
         }
+    }
+}
     }
 }
